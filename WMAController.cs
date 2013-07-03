7077333fa8029b570ab2f32f7f85bc41847ac0a7
Tu Pham <tupham@imt.com.vn>
2013-07-03 10:37:05 +0700

    Implement WMAController.cs, WMVController.cs

---
+++ WMAController.cs
@@ -0,0 +1,10 @@
+using System;
+namespace FileServer.Controller
+{
+	public class WMAController
+	{
+		public WMAController()
+		{
+		}
+	}
+}
---
+++ WMVController.cs
@@ -0,0 +1,11 @@
+using System;
+namespace FileServer.Controller
+{
+	public class WMVController
+	{
+		public WMVController()
+		{
+		
+		}
+	}
+}
